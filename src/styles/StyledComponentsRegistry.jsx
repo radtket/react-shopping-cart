cf4909896b5cc4dceb366ce59cc77529db22cc37
Taylor Radtke <taylorradtke@gmail.com>
2024-12-31 11:16:54 -0600

    init

---
+++ src/styles/StyledComponentsRegistry.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useServerInsertedHTML } from "next/navigation";
+import PropTypes from "prop-types";
+import { useRef } from "react";
+import {
+  ServerStyleSheet,
+  StyleSheetManager,
+  ThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
+import theme from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default function StyledComponentsRegistry({ children }) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+  const styledComponentsStyleSheet = useRef(new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.current.getStyleElement();
+    styledComponentsStyleSheet.current.instance.clearTag();
+    return styles;
+  });
+
+  if (typeof window !== "undefined") {
+    return children;
+  }
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.current.instance}>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </StyleSheetManager>
+  );
+}
+
+StyledComponentsRegistry.propTypes = {
+  children: PropTypes.node.isRequired,
+};
---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+const colors = {
+  primary: "#1b1a20",
+  secondary: "#eabf00",
+};
+
+const breakpoints = {
+  mobile: "480px",
+  tablet: "768px",
+  desktop: "1025px",
+};
+
+const theme = {
+  colors,
+  breakpoints,
+};
+
+export default theme;
