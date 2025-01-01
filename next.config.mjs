fbc127e2c902007e9672795d13b25e1d076da03b
Taylor Radtke <taylorradtke@gmail.com>
2024-12-31 20:57:07 -0600

    basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/react-shopping-cart",
   compiler: {
     styledComponents: true,
-  }
+  },
 };
 
 export default nextConfig;
